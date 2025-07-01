622c96a5967a796fa57845b5cef4f9b5661680e5
yosef_hp <yaamr2002@gmail.com>
2025-07-02 00:46:31 +0300

    card ui updated

--- src/app/Components/mooviecard/mooviecard.ts
+++ src/app/Components/mooviecard/mooviecard.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-mooviecard',
-  imports: [CommonModule],
+  imports: [CommonModule,CardModule,ButtonModule],
   templateUrl: './mooviecard.html',
   styleUrl: './mooviecard.scss'
 })
